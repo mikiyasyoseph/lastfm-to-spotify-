a255897f037e9ac40c59f1890217757782be0725
Mikiyas <122575428+mikiyasyoseph@users.noreply.github.com>
2024-03-11 23:46:16 -0500

    Fixed time period fetching

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import requests
 from datetime import datetime
-import config
+from config import user, apikey
+from pprint import pprint
 
-start_date = datetime(2024, 2, 1)  # Start date (YYYY, MM, DD)
-end_date = datetime(2024, 2, 29)  # End date (YYYY, MM, DD)
-start_date_str = start_date.strftime('%Y-%m-%d')
-end_date_str = end_date.strftime('%Y-%m-%d')
- 
-lastfm_endpoint = f'http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&user={user}&api_key={apikey}&format=json&date={start_date_str},{end_date_str}&limit=50'
+start_date = datetime(2023, 2, 1)  
+end_date = datetime(2023, 2, 28)
+from_time = int(start_date.timestamp())
+to_time = int(end_date.timestamp())
 
+lastfm_endpoint = f'http://ws.audioscrobbler.com/2.0/?method=user.getweeklytrackchart&user={user}&api_key={apikey}&format=json&limit=10&from={from_time}&to={to_time}'
 response = requests.get(lastfm_endpoint)
+#pprint(response.json())
 
-top_tracks_data = response.json()['toptracks']
-top_tracks = top_tracks_data['track']
-
-for track in top_tracks:
-    track_name = track['name']
-    artist_name = track['artist']['name']
-    play_count = track['playcount']
-    print(f"{track_name} by {artist_name} (Played {play_count} times)")
+if response.status_code == 200:
+    top_tracks_data = response.json()['weeklytrackchart']
+    top_tracks = top_tracks_data['track']
+    for track in top_tracks:
+        track_name = track['name']
+        artist_name = track['artist']['#text']
+        play_count = track['playcount']
+        print(f"{track_name} by {artist_name} ({play_count} scrobbles)")
+else:
+    print(f"Failed to fetch top tracks for the specified time period. Status code: {response.status_code}")
